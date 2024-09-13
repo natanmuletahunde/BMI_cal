c24ccab5f9c25f3c45f4a44de5c31f762e009f8e
pivnati <natanmuleta911@gmail.com>
2024-09-12 20:44:45 -0700

    convert the cente meter and in to meter

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -33,7 +33,7 @@ class _BMICalculatorPageState extends State<BMICalculatorPage> {
     double height = double.parse(heightController.text) / 100;
     double weight = double.parse(weightController.text);
     double calculatedBMI = weight / pow(height, 2);
-    
+
     setState(() {
       bmi = calculatedBMI;
       bmiResult = bmi!.toStringAsFixed(2);
@@ -70,115 +70,110 @@ class _BMICalculatorPageState extends State<BMICalculatorPage> {
         centerTitle: true,
         elevation: 10.0,
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.greenAccent, Colors.lightBlueAccent],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            SizedBox(height: 20.0),
-            Text(
-              'Calculate Your BMI',
-              style: TextStyle(
-                fontSize: 32.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 20.0),
-            Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              elevation: 5.0,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    TextField(
-                      controller: heightController,
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                        labelText: 'Height in cm',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 16.0),
-                    TextField(
-                      controller: weightController,
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                        labelText: 'Weight in kg',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                      ),
-                    ),
-                  ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              SizedBox(height: 20.0),
+              Text(
+                'Calculate Your BMI',
+                style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
                 ),
+                textAlign: TextAlign.center,
               ),
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: calculateBMI,
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(vertical: 15.0), backgroundColor: Colors.green,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                shadowColor: Colors.black,
-                elevation: 10,
-              ),
-              child: Text(
-                'Calculate BMI',
-                style: TextStyle(fontSize: 18.0, color: Colors.white),
-              ),
-            ),
-            SizedBox(height: 20.0),
-            if (bmi != null) ...[
+              SizedBox(height: 20.0),
               Card(
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(15.0),
                 ),
                 elevation: 5.0,
-                color: Colors.tealAccent,
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: Column(
                     children: [
-                      Text(
-                        'Your BMI is $bmiResult',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
+                      TextField(
+                        controller: heightController,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          labelText: 'Height in cm',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
                         ),
-                        textAlign: TextAlign.center,
                       ),
-                      SizedBox(height: 10.0),
-                      Text(
-                        '$bmiDescription',
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.green[900],
-                          fontWeight: FontWeight.bold,
+                      SizedBox(height: 16.0),
+                      TextField(
+                        controller: weightController,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          labelText: 'Weight in kg',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
                         ),
-                        textAlign: TextAlign.center,
                       ),
                     ],
                   ),
                 ),
               ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: calculateBMI,
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 15.0), backgroundColor: Colors.green,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  shadowColor: Colors.black,
+                  elevation: 10,
+                ),
+                child: Text(
+                  'Calculate BMI',
+                  style: TextStyle(fontSize: 18.0, color: Colors.white),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              if (bmi != null) ...[
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  elevation: 5.0,
+                  color: Colors.tealAccent,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Your BMI is $bmiResult',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(height: 10.0),
+                        Text(
+                          '$bmiDescription',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.green[900],
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ],
-          ],
+          ),
         ),
       ),
     );
